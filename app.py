80115c5eaa539a15c913f555e315f096903ab224
qianhao <ww6652890@gmail.com>
2019-10-18 11:21:48 -0500

    two pages about manmage, choose one

--- app.py
+++ app.py
@@ -7,9 +7,18 @@ app = Flask(__name__)
 def hello_world():
     return render_template('index.html')
 
-# @app.route('/manpage')
-# def manpage():
-#     retrun render_template('manpage.html')
+@app.route('/manpage1')
+def manpage1():
+    return render_template('manpage.html')
+
+@app.route('/manpage2')
+def manpage2():
+    return render_template('manpage_2.html')
+
+@app.route('/manpage3')
+def manpage3():
+    return render_template('manpage_3.html')
 
 if __name__ == '__main__':
+    # app.run(debug=True)
     app.run()
